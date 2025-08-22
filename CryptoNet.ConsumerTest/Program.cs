ffc98e86654f0694aa2641cf293733d3ea23f80b
Maytham Fahmi <9260645+maythamfahmi@users.noreply.github.com>
2025-08-22 12:23:06 +0200

    #147 adding consumber project

---
+++ CryptoNet.ConsumerTest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptoNet.ConsumerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is only to test if the latest Nuget package is consumable for nuget.net");
+            //todo: implement a test that uses the CryptoNet library
+        }
+    }
+}
